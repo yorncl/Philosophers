faba7dee1e6c1b88682837010d68fc9b44f641c7
mclaudel <mclaudel@student.42.fr>
2020-08-03 14:02:00 +0200

    nprmed header

--- philo_one/inc/philo_one.h
+++ philo_one/inc/philo_one.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_one.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/03 13:51:13 by user42            #+#    #+#             */
+/*   Updated: 2020/08/03 14:01:26 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_ONE_H
 # define PHILO_ONE_H
 
@@ -6,7 +18,7 @@
 # include <sys/time.h>
 # include <pthread.h>
 
-typedef struct	s_p1 t_p1;
+typedef struct s_p1		t_p1;
 
 typedef struct	s_params
 {
@@ -17,33 +29,31 @@ typedef struct	s_params
 	pthread_t		monitor;
 }				t_params;
 
-typedef struct	s_p1
+struct			s_p1
 {
-	int				start;
-	int				nb;
-	unsigned int	todie;
-	unsigned int	toeat;
-	unsigned int	tosleep;
-	int				musteat;
-	int				someonedied;
-	int				someonefull;
-	pthread_t		*threads;
-	pthread_mutex_t isdying;
-	pthread_mutex_t print_mutex;
-	pthread_mutex_t *forks;
-	t_params		*params;
-	struct timeval	time_start;
-	unsigned long int timestampstart;
-	struct timeval	time_now;
-}				t_p1;
-
-
+	int					start;
+	int					nb;
+	unsigned int		todie;
+	unsigned int		toeat;
+	unsigned int		tosleep;
+	int					musteat;
+	int					someonedied;
+	int					someonefull;
+	pthread_t			*threads;
+	pthread_mutex_t		isdying;
+	pthread_mutex_t		print_mutex;
+	pthread_mutex_t		*forks;
+	t_params			*params;
+	struct timeval		time_start;
+	unsigned long int	timestampstart;
+	struct timeval		time_now;
+};
 
 int				ft_strlen(char *str);
 void			ft_putstr_fd(int fd, char *str, int len);
 void			ft_putunsigned_fd(int fd, unsigned long long int nb);
 unsigned int	ft_atoi(char *s);
-unsigned int 	get_timestamp(t_p1 *p);
+unsigned int	get_timestamp(t_p1 *p);
 
 int				init_threads(t_p1 *p);
 void			free_params(t_p1 *p);
